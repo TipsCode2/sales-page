b0595767982130ea727b3fcc014fae2a829af99a
anapaulasuassuna <anapaulaenf.ce@gmail.com>
2024-11-07 11:14:39 -0300

    teste 7 mudei o ssg-build

--- ssg/service-worker.js
+++ ssg/service-worker.js
@@ -5,7 +5,7 @@ self.context = {
     "development": false,
     "production": true,
     "mode": "ssg",
-    "key": "8db6a365cb2ecd90dbfbc2088acade3fa6abb773",
+    "key": "d2f3f2504b6a381b569af55affa48a3987f6a5b4",
     "name": ""
   },
   "project": {
